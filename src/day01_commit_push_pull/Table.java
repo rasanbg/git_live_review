ede42d66545c25cf90e7056ce0b1fc114ece1745
rasanbg <153374653+rasanbg@users.noreply.github.com>
2025-03-02 17:16:37 +0100

    Added Table class with multiplicationTable metod

---
+++ src/day01_commit_push_pull/Table.java
@@ -0,0 +1,24 @@
+package day01_commit_push_pull;
+
+public class Table {
+    public static void main(String[] args) {
+        multiplicationTable(5);
+    }
+
+    /*
+    Method: multiplicationTable
+    Parameter: num
+    Returun: Void
+    prints multiplication table
+     */
+
+    public static void multiplicationTable(int num) {
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+
+        }
+
+    }
+}
+
+
