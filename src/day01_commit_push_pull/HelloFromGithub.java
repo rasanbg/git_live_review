271f1b43eed75c5a81a159f9819159fe0c4d2e88
rasanbg <153374653+rasanbg@users.noreply.github.com>
2025-03-16 15:24:40 +0100

    Added PullDemo

--- src/day01_commit_push_pull/HelloFromGithub.java
+++ src/day01_commit_push_pull/HelloFromGithub.java
@@ -1,5 +1,7 @@
 package day01_commit_push_pull;
 
 public class HelloFromGithub {
+    public static void main(String[] args) {
 
+    }
 }
---
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -0,0 +1,10 @@
+package day02_clone_pull_rollback;
+
+public class PullDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Local");
+
+    }
+}
