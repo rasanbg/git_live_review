eeb6f1073fbb910cf15c02a54948e0d2e62efad8
rasanbg <153374653+rasanbg@users.noreply.github.com>
2025-03-16 16:25:12 +0100

    Update PullDemo.java-line 11

--- src/day02_clone_pull_rollback/PullDemo.java
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -1,7 +1,5 @@
 package day02_clone_pull_rollback;
-
 public class PullDemo {
-
     public static void main(String[] args) {
 
         System.out.println("Hello from Local");
@@ -9,5 +7,7 @@ public class PullDemo {
         System.out.println("Hello from teammate who pushed to Github");
 
         System.out.println("Hello from another teammate in Github");
+
+        System.out.println("Hello again from Another in GIthub at line 11");
     }
 }
